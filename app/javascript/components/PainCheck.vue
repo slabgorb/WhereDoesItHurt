7398b1aaaea4f24665ab9f318d749501a0da3552
Keith Avery <slabgorb@gmail.com>
2022-09-14 10:36:38 -0400

    remove dep. styling in favor of vuetify

--- app/javascript/components/PainCheck.vue
+++ app/javascript/components/PainCheck.vue
@@ -2,8 +2,8 @@
     <v-card>
         <v-card-title>Pain Check</v-card-title>
         <v-card-subtitle>record your current pain level</v-card-subtitle>
-        <v-img src="pain_chart.jpg" :aspect-ratio="1/1">
-
+        <v-img src="app/javascript/assets/images/pain_chart.jpg" :aspect-ratio="1/1">
+           
         </v-img>
     </v-card>
 </template>
