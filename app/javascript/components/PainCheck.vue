60a40979d4927af2ecdf38066a8d32533c4d881e
Keith Avery <slabgorb@gmail.com>
2022-09-15 08:06:49 -0400

    drawable canvas

--- app/javascript/components/PainCheck.vue
+++ app/javascript/components/PainCheck.vue
@@ -1,131 +1,105 @@
 <template>
-    <v-card>
-        <v-card-title>Pain Check</v-card-title>
-        <v-card-subtitle>record your current pain level</v-card-subtitle>
-        <canvas ref="painRecord" class="pain-record">
-        </canvas>
-    </v-card>
+  <v-card>
+    <v-card-title>Pain Check</v-card-title>
+    <v-card-subtitle>record your current pain level</v-card-subtitle>
+    <canvas ref="painRecord" class="pain-record">
+    </canvas>
+  </v-card>
 </template>
 
 
 <style scoped>
-    .pain-record {
-        width: 500px;
-        height: 500px;
-        background-position: top;
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
+.pain-record {
+  width: 500px;
+  height: 500px;
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 </style>
 
 <script setup lang="ts">
-
 import imgUrl from '@/assets/images/pain_chart.jpg'
 import { onMounted, ref } from 'vue';
 const painRecord = ref<HTMLCanvasElement>();
-let prevX :number, prevY :number, currX :number, currY :number;
-prevX = prevY = currX = currY = 0;
-let flag = false;
-let dotFlag = false;
+
+let currentlyPainting = false;
+let context: CanvasRenderingContext2D | null = null;
+
+function getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+    y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+  };
+}
+
 onMounted(() => {
-    if (painRecord != null && painRecord.value != null) {
-        const canvas = painRecord.value;
-        canvas.addEventListener("mousemove", function (e) {
-            findxy(canvas,'move', e)
-        }, false);
-        canvas.addEventListener("mousedown", function (e) {
-            findxy(canvas,'down', e)
-        }, false);
-        canvas.addEventListener("mouseup", function (e) {
-            findxy(canvas,'up', e)
-        }, false);
-        canvas.addEventListener("mouseout", function (e) {
-            findxy(canvas,'out', e)
-        }, false);
-        painRecord.value.style.backgroundImage = `url(${imgUrl})`;
+  const canvas = painRecord.value!;
+  if (canvas == null) {
+    return;
+  }
+  canvas.addEventListener("mousemove", function (e) {
+    if (currentlyPainting) {
+      draw(e);
     }
-})
+  }, false);
+  canvas.addEventListener("mousedown", function (e) {
+    currentlyPainting = true;
+  }, false);
+  canvas.addEventListener("mouseup", function (e) {
+    currentlyPainting = false;
+  }, false);
+  canvas.style.backgroundImage = `url(${imgUrl})`;
+  context = painRecord.value!.getContext('2d');
+});
 
-function draw(ctx: CanvasRenderingContext2D) {
-    ctx.beginPath();
-    ctx.moveTo(prevX, prevY);
-    ctx.lineTo(currX, currY);
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 2;
-    ctx.stroke();
-    ctx.closePath();
+const cursorSize = ref(4);
+
+function draw(evt: MouseEvent) {
+  const canvas = painRecord.value!;
+  var pos = getMousePos(canvas, evt);
+  const offset = cursorSize.value / 2;
+  context!.fillStyle = "#000000";
+  context!.fillRect(pos.x - offset, pos.y - offset, cursorSize.value, cursorSize.value);
 }
-
-function findxy(canvas: HTMLCanvasElement, res :string, e :MouseEvent) {
-    const ctx = canvas.getContext("2d");
-    if (ctx == null) {
-        return;
-    }
-    if (res == 'down') {
-        prevX = currX;
-        prevY = currY;
-        currX = e.clientX - canvas.offsetLeft;
-        currY = e.clientY - canvas.offsetTop;
-        console.log(ctx);
-        flag = true;
-        dotFlag = true;
-        if (dotFlag && ctx) {
-            ctx.beginPath();
-            ctx.fillStyle = 'red';
-            ctx.fillRect(currX, currY, 2, 2);
-            ctx.closePath();
-            dotFlag = false;
-        }
-    }
-    if (res == 'up' || res == "out") {
-        flag = false;
-    }
-    if (res == 'move') {
-        if (flag) {
-            prevX = currX;
-            prevY = currY;
-            currX = e.clientX - canvas.offsetLeft;
-            currY = e.clientY - canvas.offsetTop;
-            draw(ctx);
-        }
-    }
-}
-
 
 const internalityList = [
-    {
-        value: 0,
-        label: "internal",
-    },
-    {
-        value: 1,
-        label: "external",
-    },
+  {
+    value: 0,
+    label: "internal",
+  },
+  {
+    value: 1,
+    label: "external",
+  },
 ];
 
 const sensationList = [
-    {
-        value: 0,
-        label: "sharp",
-    },
-    {
-        value: 1,
-        label: "dull",
-    },
-    {
-        value: 2,
-        label: "burning",
-    },
-    {
-        value: 3,
-        label: "tingling",
-    },
-    {   value: 4,
-        label: "numbness",
-    },
-    {   value: 5,
-        label: "other",
-    },
+  {
+    value: 0,
+    label: "sharp",
+  },
+  {
+    value: 1,
+    label: "dull",
+  },
+  {
+    value: 2,
+    label: "burning",
+  },
+  {
+    value: 3,
+    label: "tingling",
+  },
+  {
+    value: 4,
+    label: "numbness",
+  },
+  {
+    value: 5,
+    label: "other",
+  },
 ];
 
 const sensation = ref('');
